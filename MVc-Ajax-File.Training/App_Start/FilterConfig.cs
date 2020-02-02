01e2fe62e3c50c8795a785d4ca96e32f10ca4263
icadir <ismailcadir@gmail.com>
2020-02-03 01:08:08 +0300

    Add project files.

---
+++ MVc-Ajax-File.Training/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVc_Ajax_File.Training
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MVc-Ajax-File.Training/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVc_Ajax_File.Training.MvcApplication" Language="C#" %>
